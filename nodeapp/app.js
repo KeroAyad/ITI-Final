2590d016c82237795d36b506c3c72349be8e8f1b
Kerolos Ayad <kerolos.ayad@outlook.com>
2022-11-03 02:08:09 +0200

    webhook test commit

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -4,7 +4,3 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('<h1>Hello World<h1/><br><h2>by: Kero</h2>')
   })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
